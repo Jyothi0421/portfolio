e952068d400a66fe681e44f74e61551073331612
Jyothi Reddy <129441074+Jyothi0421@users.noreply.github.com>
2023-04-02 17:40:20 +0530

    Add files via upload

---
+++ task2/script.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+
+    $('#menu').click(function(){
+        $(this).toggleclass('fa-times');
+        $('header').toggleClass('toggle');
+    });
+
+    $(window).on('scroll load',function(){
+        $('#menu').removeclass('fa-times');
+        $('header').removeClass('toggle');
+    });
+
+    $('a[href*="#"]').on('click',function(e){
+        e.preventDefault();
+
+        $('html, body').animate({
+
+            scrollTop : $($(this).attr('href')).offset().top,
+        },
+          500,
+          'linear'
+        );
+
+
+    });
+});
